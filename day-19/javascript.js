28197e2c6551e50e664df91996481e8f60cdcf69
Manohar129 <manoharbairaboyina23@gmail.com>
2025-11-06 09:34:00 +0530

    day-19 task-JavaScript variables and data types

---
+++ day-19/javascript.js
@@ -0,0 +1,58 @@
+let Name ="manohar";
+
+
+console.log(Name);
+
+let age = 22;
+
+console.log(age);
+
+let isStudent = true;
+
+console.log(isStudent);
+
+let address;
+
+console.log(address);   
+address = "123 Main St, Cityville";
+
+console.log(address);
+address = null;
+
+console.log(address);
+const pi = 3.14;
+
+console.log(pi);
+// pi = 3.14159; // This will cause an error
+// console.log(pi);
+var score = 100;
+
+console.log(score);
+score = 150;
+console.log(score);
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+let x = 10;
+let y = 5;
+let sum = x + y;
+console.log("Sum:", sum);
+
+let pcity={ M:"New York", N:"Los Angeles", O:"Chicago"};
+
+console.log(pcity);
+console.log(pcity.M);
+console.log(pcity.N);
+console.log(pcity.O);
+pcity.M="San Francisco";
+console.log(pcity.M);
+console.log(pcity);
+
+let fruits = ["Apple", "Banana", "Cherry"];
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
