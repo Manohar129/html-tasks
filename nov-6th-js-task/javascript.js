edb55781e73b943a924dfb0313d29bcd12bdfcd5
Manohar129 <manoharbairaboyina23@gmail.com>
2025-11-06 16:37:21 +0530

    js nov 6th task

---
+++ nov-6th-js-task/javascript.js
@@ -0,0 +1,60 @@
+
+ // Write js code 
+  // Using Const
+
+   // 1. Declare three variables (using var, let, const) to store your city, age, and country.
+  // 2. Try reassigning all of them.
+   // 3. Observe which ones work and which give an error.
+
+  // using Let
+  //  1. Create a variable using let to store your age.
+  //  2. Print it in the console.
+  //  3. Try changing the value (reassign).
+  //  4. Print again.
+  //  5. Try redeclaring it and see what happens.
+
+   // Using var
+   // 1. Create a variable using var to store your name.
+   // 2. Print it in the console.
+   // 3. Change its value and print again.
+
+   var city = "Guntur"; // using var
+   let age = 25; // using let
+   const country = "India"; // using const
+
+   // Reassigning variables
+   city = "Hyderabad"; // 
+    age = 26; // works
+   //  country = "USA";
+    
+    console.log("City:", city); // prints "City: Hyderabad"
+    console.log("Age:", age); // prints "Age: 26"
+    // console.log("Country:", country); // Error: Assignment to constant variable.
+
+
+    //1. using Let
+    let myage =25;  
+    //2. print myage=25
+    console.log("myage:",myage); 
+
+    //3.reassigning the value 
+    myage= 30;
+
+    //4.agin print
+    console.log("myage:",myage);
+
+    //5.redeclaring variable
+    // let myage = 20; //  'myage' has already been declare
+
+
+    //using var
+     
+    var Name= "manohar";
+
+    console.log("Name:",Name);
+
+    // changing the value
+
+    Name= "madhu"
+
+    console.log("Name:",Name)
